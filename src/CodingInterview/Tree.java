f0bb51f45ff0d28095a7c0695249a0ad3db20d3a
JH <jhliang9@gmail.com>
2022-03-15 23:19:21 +0800

    树 #114. 二叉树展开为链表

--- src/CodingInterview/Tree.java
+++ src/CodingInterview/Tree.java
@@ -49,6 +49,35 @@ public class Tree {
     }
 
 
+    /**
+     * 114. 二叉树展开为链表
+     * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+     * <p>
+     * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+     * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+     * <p>
+     * 输入：root = [1,2,5,3,4,null,6]
+     * 输出：[1,null,2,null,3,null,4,null,5,null,6]
+     *
+     * @param root 二叉树的根结点
+     */
+    public void flatten(TreeNode root) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode left = cur.left;
+                TreeNode end = left;
+                while (end.right != null) {
+                    end = end.right;
+                }
+                end.right = cur.right;
+                cur.left = null;
+                cur.right = left;
+            }
+            cur = cur.right;
+        }
+    }
+
 }
 
 /**
