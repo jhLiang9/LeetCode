d3164b169f38d74527207c711733525721da36cb
JH <jhliang9@gmail.com>
2022-03-25 00:53:00 +0800

    数学 #172. 阶乘后的零

--- src/EASY/Solution.java
+++ src/EASY/Solution.java
@@ -23,4 +23,51 @@ public class Solution {
         }
         return ret;
     }
+
+    /**
+     * 172. 阶乘后的零
+     *
+     * @param n 整数 n
+     * @return 尾随0 的数量
+     * 给定一个整数 n ，返回 n! 结果中尾随零的数量。
+     */
+    public int trailingZeroes(int n) {
+        //找2和5的数量
+        if (n == 0) return 0;
+        int start = 1;
+        int count = 0;
+        int count2 = 0;
+        int count5 = 0;
+
+        while (start++ != n) {
+            int temp = start;
+            while (temp != 0) {
+                if (temp % 10 == 0) {
+                    temp /= 10;
+                    count++;
+                } else if (temp % 5 == 0) {
+                    temp /= 5;
+                    count5++;
+                } else if (temp % 2 == 0) {
+                    temp /= 2;
+                    count2++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return count + Math.min(count2, count5);
+    }
+
+    //Solution 2
+
+    public int trailingZeroes2(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+
 }
