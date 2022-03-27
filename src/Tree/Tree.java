1fce7233c85cbc7401c71429e56dd5149cd57d35
JH <jhliang9@gmail.com>
2022-03-27 09:52:01 +0800

    树 #110. 平衡二叉树

--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -293,5 +293,28 @@ public class Tree {
     }
 
 
+    /**
+     * 110. 平衡二叉树
+     *
+     * @param root 根节点
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+
+
 }
 
