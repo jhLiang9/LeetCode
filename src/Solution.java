56f39a5bf7c3ecf0edae6df411e6f94a16f8b1ce
JH <jhliang9@gmail.com>
2022-04-02 00:24:58 +0800

    矩阵 #954. 二倍数对数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     /**
@@ -171,8 +169,29 @@ public class Solution {
         }
     }
 
+    public boolean canReorderDoubled(int[] arr) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int x : arr) {
+            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+        }
+        if (cnt.getOrDefault(0, 0) % 2 != 0) {
+            return false;
+        }
+
+        List<Integer> vals = new ArrayList<>(cnt.keySet());
+        vals.sort((a, b) -> Math.abs(a) - Math.abs(b));
+
+        for (int x : vals) {
+            if (cnt.getOrDefault(2 * x, 0) < cnt.get(x)) { // 无法找到足够的 2x 与 x 配对
+                return false;
+            }
+            cnt.put(2 * x, cnt.getOrDefault(2 * x, 0) - cnt.get(x));
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-
+        Solution s = new Solution();
+        System.out.println(s.canReorderDoubled(new int[]{1, 1, 1, 2, 2, 2}));
     }
 }
