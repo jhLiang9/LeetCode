74886d39db71c4758bcd5bd662afe0f5ebc57719
JH <jhliang9@gmail.com>
2022-05-09 23:43:03 +0800

    数组 #442. 数组中重复的数据

---
+++ src/array/Solution.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] > 0) {
+                nums[x - 1] = -nums[x - 1];
+            } else {
+                ans.add(x);
+            }
+        }
+        return ans;
+    }
+}
