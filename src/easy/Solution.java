50b22eb40f41d81f2f6c88d74e56cd0cac88749b
JH <jhliang9@gmail.com>
2022-05-13 00:33:59 +0800

    Easy #944. 删列造序

--- src/easy/Solution.java
+++ src/easy/Solution.java
@@ -455,6 +455,27 @@ public class Solution {
         return res;
     }
 
+    /**
+     * 944. 删列造序
+     *
+     * @param strs 字符串数组
+     * @return 删除的列数
+     * 思路:列->行
+     */
+    public int minDeletionSize(String[] strs) {
+        int row = strs.length;
+        int col = strs[0].length();
+        int ans = 0;
+        for (int j = 0; j < col; ++j) {
+            for (int i = 1; i < row; ++i) {
+                if (strs[i - 1].charAt(j) > strs[i].charAt(j)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         Solution s = new Solution();
